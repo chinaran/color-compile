a8ab13270590301b935c1ac2191e20068798331a
Randy Wang <gchinaran@gmail.com>
2015-04-02 10:43:29 +0800

    add support Chinese error, like 错误, 附注, 警告

--- out_color_info.c
+++ out_color_info.c
@@ -25,15 +25,40 @@
 #define WARNING	"warning:"
 #define ERROR	"error:"
 #define NOTE	"note:"
+#define WARNING_CN	"警告："
+#define ERROR_CN	"错误："
+#define NOTE_CN	"附注："
+#define MARK_NUM	6
+
+#define TRUE	1
+#define FALSE	0
+
+struct mark_st
+{
+	const char *mark;
+	const char *color;
+	int b_cn;
+};
+
+static struct mark_st Mark[MARK_NUM] = 
+{
+	{WARNING, WARNING_COLOR, FALSE},
+	{ERROR, ERROR_COLOR, FALSE},
+	{NOTE, NOTE_COLOR, FALSE},
+	{WARNING_CN, WARNING_COLOR, TRUE},
+	{ERROR_CN, ERROR_COLOR, TRUE},
+	{NOTE_CN, NOTE_COLOR, TRUE},
+};
 
 // function declare
-static void color_print_line(const char *line, const char *color);
+static void color_print_line(const char *line, const char *color, int b_cn);
 static int str_char_count(const char *str, char c);
 
 
 int main(void)
 {
 	char line[LINE_SIZE];
+	int i;
 
 	while (1)
 	{
@@ -44,25 +69,23 @@ int main(void)
 
 		// color_comile.c:35:6: warning: unused variable ‘t’ [-Wunused-variable]
 		// xx.c:11:41: fatal error: include/xx.h: No such file or directory
-		if (str_char_count(line, ':') < 4)
+		// test.c:24:2: 错误： ‘a’未声明(在此函数内第一次使用)
+		if (str_char_count(line, ':') < 3)
 		{
 			printf("%s", line);
 			continue;
 		}
 
-		if (NULL != strstr(line, WARNING))
+		for (i = 0; i < MARK_NUM; ++i)
 		{
-			color_print_line(line, WARNING_COLOR);
+			if (NULL != strstr(line, Mark[i].mark))
+			{
+				color_print_line(line, Mark[i].color, Mark[i].b_cn);
+				break;
+			}
 		}
-		else if (NULL != strstr(line, ERROR))
-		{
-			color_print_line(line, ERROR_COLOR);
-		}
-		else if (NULL != strstr(line, NOTE))
-		{
-			color_print_line(line, NOTE_COLOR);
-		}
-		else
+
+		if (MARK_NUM == i)
 		{
 			printf("%s", line);
 		}
@@ -71,7 +94,7 @@ int main(void)
 	return 0;
 }
 
-static void color_print_line(const char *line, const char *color)
+static void color_print_line(const char *line, const char *color, int b_cn)
 {
 	// color_comile.c:35:6: warning: unused variable ‘t’ [-Wunused-variable]
 	char *filename;
@@ -92,7 +115,15 @@ static void color_print_line(const char *line, const char *color)
 	snprintf(buf, col - row, "%s", row + 1);
 	printf("%s%s%s:", COL_COLOR, buf, COLOR_END);
 
-	msg = index(col + 1, ':');
+	if (FALSE == b_cn)
+	{
+		msg = index(col + 1, ':');
+	}
+	else
+	{
+		msg = strstr(col + 1, "：");
+	}
+	
 	snprintf(buf, msg - col, "%s", col + 1);
 	printf("%s%s%s:", color, buf, COLOR_END);
 
